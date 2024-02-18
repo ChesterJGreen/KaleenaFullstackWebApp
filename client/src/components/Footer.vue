20a8d9ea27f9fdc2c7d5800f4ef2116b07c84f28
ChesterJGreen <seejarom@gmail.com>
2024-02-17 18:03:23 -0700

    updating frontend

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="container-fluid mt-5">
+        <div class="row pinkSquareBorder">
+            <div class="col-10 offset-1 pinkBorder">
+                <div class="row">
+                    <div class="col-3 p-5">
+                        <img src="src\assets\img\LogoTransparentEnlargedCropped.png" width="300">
+                    </div>
+                    <div class="col-6 p-5 text-center">
+                        <div class="row">
+                            <div class="col-12 pt-5" id="SweetBites">
+                                <p class="ThanksText cardo-font">Thanks for stopping by!</p>
+                                <button class="btn rounded p-4 keepInTouch cardo-font" data-toggle="modal"
+                                    data-target="#newsletterModal">Let's keep in touch</button>
+                                <img src="src\assets\img\Arrow.png" id="Arrow">
+                                <p id="SBText" class="rochester-font">Sweet Bites</p>
+                                <p id="NewsLetter" class="volk-font">Newsletter</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 pt-5">
+                        <img src="src\assets\img\StampPic.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-12">
+                <ul class="navbar justify-content-center justify-content-end">
+                    <ul>
+                        <a href="https://www.facebook.com/kaleena.shreeve"><img width="25"
+                                src="src\assets\img\f_logo_RGB-Black_144.png"></a>
+                    </ul>
+                    <ul>
+                        <a href="https://www.instagram.com/kaleenashreeve1/"><img width="25"
+                                src="src\assets\img\Instagram_Glyph_Black.png"></a>
+                    </ul>
+                    <ul>
+                        <a href="https://www.instagram.com/kaleenashreeve1/"><img width="25"
+                                src="src\assets\img\PinterestLogo.png"></a>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+        <div class="row footer-text volk-font">
+            <div class="col-9 offset-1">
+                <div class="row">
+                    <ul class="col-2 offset-2 text-end">
+                        <router-link :to="{ name: 'About' }" class="text-dark selectable text-uppercase">
+                            About Me
+                        </router-link>
+                    </ul>
+                    <ul class="col-2 text-center">
+                        <router-link :to="{ name: 'Books' }" class="text-dark selectable text-uppercase">
+                            Books
+                        </router-link>
+                    </ul>
+                    <ul class="col-1">
+                        <router-link :to="{ name: 'Journeys' }" class=" text-dark selectable text-uppercase">
+                            Extras
+                        </router-link>
+                    </ul>
+                    <ul class="col-2 text-center">
+                        <router-link :to="{ name: 'Blog' }" class="text-dark selectable text-uppercase">
+                            Blog
+                        </router-link>
+                    </ul>
+                    <ul class="col-1">
+                        <router-link :to="{ name: 'Contact' }" class="text-dark selectable text-uppercase">
+                            Contact
+                        </router-link>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-2">
+                <span class="copyright-text">© 2023 Kaleena Shreeve</span>
+            </div>
+        </div>
+
+    </div>
+</template>
+  
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+  
+<style scoped>
+a:hover {
+    text-decoration: none;
+}
+
+.nav-link {
+    text-transform: uppercase;
+}
+
+.navbar-nav .router-link-exact-active {
+    border-bottom: 2px solid var(--bs-success);
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+button {
+    font-size: x-large;
+}
+.pinkBorder {
+    border: solid 12px;
+    border-color: rgb(250, 183, 224);
+    border-radius: 12px;
+}
+
+.ThanksText {
+    font-size: 300%;
+}
+
+#SweetBites {
+    position: relative;
+}
+
+#Arrow {
+    position: absolute;
+    top: 10em;
+    left: 6em;
+    width: 21%;
+    transform: scaleY(-1);
+}
+
+#SBText {
+    font-size: 400%;
+}
+
+.keepInTouch {
+    background-color: rgb(201 210 188);
+    font-size: xx-large;
+}
+
+#NewsLetter {
+    font-size: 300%;
+}
+
+.footer-text {
+    font-size: 175%;
+}
+
+.copyright-text {
+    font-size: 70%;
+}
+</style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,75 +1,133 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+  <div class="container-fluid bg-white">
+    <nav class="row navbar navbar-expand-lg">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-3 p-2 offset-9" id="SweetBites">
+            <img src="src\assets\img\Arrow.png" id="Arrow">
+            <span class="text-start rochester-font" id="SBText">Sweet Bites</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-2 ms-5">
+            <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+              <div class="d-flex flex-column align-items-center">
+                <img src="src\assets\img\LogoTransparentEnlargedCropped.png" class="rounded-pill w-100" alt="">
+              </div>
+            </router-link>
+          </div>
+          <div class="col-9">
+            <div class="collapse navbar-collapse pt-3" id="navbarText">
+              <ul class="navbar-nav justify-content-right justify-content-end">
+                <ul class="navbar-nav me-auto text-dark volk-font font-30px d-flex flex-row">
+                  <div class="row ">
+                    <div class="col-9 d-flex">
+                      <Div class="row">
+                      <li class="col-3">
+                        <router-link :to="{ name: 'About' }" class="router btn text-dark selectable text-uppercase">
+                          About Me
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Books' }" class="router btn text-dark selectable text-uppercase">
+                          Books
+                        </router-link>
+                      </li>
+                      <li class="col-3">
+                        <router-link :to="{ name: 'Journeys' }" class="router btn text-dark selectable text-uppercase">
+                          Journeys
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Blog' }" class="router btn text-dark selectable text-uppercase">
+                          Blog
+                        </router-link>
+                      </li>
+                      <li class="col-2">
+                        <router-link :to="{ name: 'Contact' }" class="router btn text-dark selectable text-uppercase">
+                          Contact
+                        </router-link>
+                      </li>
+                    </Div>
+                    </div>
+                    <div class="col-2 d-flex ms-5">
+                      <li>
+                        <a href="https://www.facebook.com/kaleena.shreeve"><img width="35"
+                            src="src\assets\img\f_logo_RGB-Black_144.png"></a>
+                      </li>
+                      <li class="mx-2">
+                        <a href="https://www.instagram.com/kaleenashreeve1/"><img width="35"
+                            src="src\assets\img\Instagram_Glyph_Black.png"></a>
+                      </li>
+                      <li class="me-2">
+                        <a href="https://www.pinterest.com/kaleenashreeve/"><img width="35"
+                            src="src\assets\img\P-Badge-Cosmicore-Black-900.png"></a>
+                      </li>
+                      <li>
+                        <span class="p-1 ">
+                          <button class="btn text-white pink-button rounded-pill volk-font font-30px p-1.5 ms-3" data-toggle="modal"
+                            data-target="#newsletterModal">Newsletter</button>
+                        </span>
+                      </li>
+                    </div>
+                  </div>
+                </ul>
+              </ul>
+            </div>
+          </div>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+          </button>
+        </div>
       </div>
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <div>
-        <button class="btn text-light" @click="toggleTheme"><i class="mdi" :class="theme == 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></i></button>
-      </div>
-      <Login />
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
-import { loadState, saveState } from '../utils/Store.js';
-import Login from './Login.vue';
 export default {
   setup() {
-
-    const theme = ref(loadState('theme') || 'light')
-
-    onMounted(() => {
-      document.documentElement.setAttribute('data-bs-theme', theme.value)
-    })
-
-    return {
-      theme,
-      toggleTheme() {
-        theme.value = theme.value == 'light' ? 'dark' : 'light'
-        document.documentElement.setAttribute('data-bs-theme', theme.value)
-        saveState('theme', theme.value)
-      }
-    }
+    return {};
   },
-  components: { Login }
-}
+};
 </script>
 
 <style scoped>
 a:hover {
   text-decoration: none;
 }
-
 .nav-link {
   text-transform: uppercase;
 }
-
 .navbar-nav .router-link-exact-active {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
-
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
+.font-30px {
+  font-size: 30px;
+}
+.router {
+  font-size: 85%;
+}
+button {
+  font-family: cursive;
+}
+#SweetBites {
+  position: relative;
+}
+#Arrow {
+  position: absolute;
+  top: .5em;
+  left: 8em;
+  width: 15%;
+}
+#SBText {
+  position: absolute;
+  top: -0.25em;
+  left: 5.25em;
+  font-size: xx-large;
 }
 </style>
