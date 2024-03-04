4f0709f12c3038e81ea3c68a770aca3036febd01
ChesterJGreen <seejarom@gmail.com>
2024-03-04 15:11:40 -0700

    books page template almost compete

--- client/src/pages/BookPage.vue
+++ client/src/pages/BookPage.vue
@@ -15,30 +15,280 @@
           </div>
         </div>
       </div>
-
-
-
-
     </div>
     <div class="row">
       <div class="col-12 text-center">
-        <span class="xxx-large">Lonely Cowboy Hearts Series</span>
+        <span class="xxx-large prata-font">Lonely Cowboy Hearts Series</span>
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mt-5">
         <div class="row">
-          <div class="col-3 text-center">
-           <p><span class="rochester-font x-large">QR codes</span></p>
+          <div class="col-3 text-center mt-5">
+           <p><span class="playlist-font xxx-large">QR codes</span></p>
             <img src="src\assets\img\1-Homepage Pink QR Code Heart.png" width="100" >
           </div>
-          <div class="col-3 text-center">
-            <p><span>Romance novels with</span></p>
-            <p><span>QR codes?</span></p>
-            <p><span class="rochester-font x-large">What?</span></p>
+          <div class="col-3 text-center mt-5">
+            <p><span class="prata-font large">Romance novels with</span></p>
+            <p><span class="prata-font large">QR codes?</span></p>
+            <p><span class="playlist-font x-large">What?</span></p>
           </div>
-          <div class="col-3"></div>
-          <div class="col-3"></div>
+          <div class="col-3 text-center">
+            <div class="row">
+              <div class="col-12">
+                <p><span class="playlist-font x-large">that's right</span></p>
+                <p><span class="prata-font">The end select chapters have special</span></p>
+                <p><span class="prata-font medium">QR codes with sweet little extras</span></p>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">• Other Characters POV</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">• Recipes</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">• Songs</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">• And more!</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 text-center">
+            <img src="src\assets\img\3A Books My Photo.png" width="250">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row text-center bg-soft-blue pt-5 mt-3 pb-3">
+      <div class="col-4">
+        <div class="row text-center">
+          <div class="col-12 mt-5">
+            <span class="prata-font xx-large">Book 1</span>
+          </div>
+          <div class="col-12 my-3">
+            <img src="src\assets\img\1-HomepageMistaken Heart Logo Dark.png" width="300"> 
+          </div>
+          <div class="col-12 prata-font mt-3">
+            <span class="large">Gavin And Sarah</span>
+          </div>
+          <div class="col-12">
+                <span class="prata-font small col-12">Accidental marriage</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">Mistaken identity</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">Slow burn romance</span>
+              </div>
+              <div class="col-12">
+                <span class="prata-font small col-12">Small town</span>
+              </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <img src="src\assets\img\3A-Mistaken Heart Ipad Mockup.png" width="100%" >
+      </div>
+      <div class="col-5 mt-5">
+        <div class="row">
+          <div class="col-12 mt-5">
+            <p><span class="playlist-font large">Sarah </span><span class="x-large prata-font">loves the storybook romances</span></p>
+                <p><span class="prata-font large">and <span class="playlist-font x-large">Gavin</span> fits the modle of her <span class="playlist-font x-large">perfect</span></span></p>
+                <p><span class="prata-font large">fantasy. So why fight to get out of a</span></p>
+                <p><span class="prata-font large"><span class="playlist-font x-large">marriage</span> to the cowboy who would do</span></p>
+                <p><span class="prata-font large">anything for her? Because he thinks</span></p>
+                <p><span class="prata-font large">she is someone else . . .</span></p>
+          </div>
+          <div class="col-12">
+            <button class="rounded-pill p-2"><a><u>Buy on Amazon</u></a></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row my-4">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-3 mt-5">
+            <img src="src\assets\img\3A-Female Character-Sarah Image .png" width="100%">
+          </div>
+          <div class="col-6">
+            <div class="row">
+              <div class="col-12 text-center">
+                <span class="playlist-font xxx-large">Sarah & Gavin</span>
+              </div>
+              <div class="col-12 text-center">
+                <img src="src\assets\img\1-Homepage Heart Divider.png" width="75%">
+              </div>
+              <div class="row text-center">
+                <div class="col-4 prata-font small">
+                  <span>"Gentle fingers below her chin</span>
+                  <span>raised her lips to his own. His</span>
+                  <span>mouth was soft and inviting and she</span>
+                  <span>reciprocated the kisses born of</span>
+                  <span>affection. Each spoke of trust,</span>
+                  <span>somethings eh had always wanted,</span>
+                  <span>but now could not afford."</span>
+                  <p class="text-end"><span class="playlist-font x-large">-Sarah</span></p>
+                </div>
+                <div class="col-4"></div>
+                <div class="col-4 prata-font small">
+                  <span>"Had I met you under different</span>
+                  <span>circumstances, I would’ve</span>
+                  <span>chased you and your beautiful</span>
+                  <span>shiny red hair and gemstone</span>
+                  <span>eyes until you would have</span>
+                  <span>sknown that there isn’t another</span>
+                  <span> woman I’d ever want.”</span>
+                  <p class="text-end"><span class="playlist-font x-large">-Gavin</span></p>
+                </div>
+                <div class="col-12 my-5 py-5">
+
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <div class="row" id="arrow-from">
+                    <div class="col-3"></div>
+                    <div class="col-5 offset-1">
+                      <div class="row">
+                        <div class="col-12 mt-5 pt-5">
+                          <span class="large prata-font">Lots more on Gavin and Sarah in</span> 
+                        </div>
+                        <div class="col-12 text-center">
+                          <span class="medium prata-font pink-font"><a><u>Extras</u></a></span> 
+                        </div>
+                      </div>                  
+                    </div>
+                    <div class="col-3 mt-5 pt-5">
+                      <img src="src\assets\img\arrow-flipped-and-turned.png" id="arrow-to-extras" width="50">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 pt-3 rounded-2 mt-5">
+            <img src="src\assets\img\3A-Male Character-Gavin Image.png" class="rounded-2" width="95%">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row bg-soft-blue">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-2">
+            <img src="" width="100">
+          </div>
+          <div class="col-8">
+            <div class="row text-center my-3">
+              <div class="col-12">
+                <span class="playlist-font xx-large">Lonely Cowboy Heart Complete Series</span>
+              </div>
+              <div class="col-12">
+                <img src="src\assets\img\1-Homepage Heart Divider.png" width="400">
+              </div>
+              <div class="col-12 prata-font small">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Gavin & Sarah</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Stratton & Shay</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Brock & Amber</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Troy & Rivers</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 mb-3">
+                <div class="row text-center prata-font small">
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Mistaken Heart Cover-Rollover for Blurb.png" width="150">
+                  </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Book 2 Mock-up.png" width="150">
+                  </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Book 3 Mock-up.png" width="150">
+                  </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A- Book 4 Mock-up.png" width="150">
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <span class="playlist-font xx-large"> Christmnas go-along Novels</span>
+              </div>
+              <div class="col-12">
+                <img src="src\assets\img\1-Homepage Heart Divider.png" width="400">
+              </div>
+              <div class="col-12">
+                <div class="row prata-font small">
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Sarah's best friend</span>
+                      </div>
+                      <div class="col-12">
+                        <span>Joan</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="row">
+                      <div class="col-12">
+                        <span>Shay's ex-boyfriend</span>
+                      </div>
+                      <div class="col-12">
+                        <span>Rhett</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                  </div>
+                  <div class="col-3">
+                  </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <img src="src\assets\img\3A-Mistletoe Wishes & Gingerbread Kisses Mockup.png" width="150">
+                      </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Mistletoe Wishes & Christmas Kisses Mockup.png" width="150">
+                  </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Mistletoe Wishes & Midnight Kisses Mockup.png" width="150">
+                  </div>
+                  <div class="col-3">
+                    <img src="src\assets\img\3A-Mistletoe Wishes & Chocolate Kisses Mockup.png" width="150">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+
+          </div>
         </div>
       </div>
     </div>
@@ -85,4 +335,50 @@ export default {
   position: relative;
   top: -40em;
 }
+.bg-soft-blue {
+  background-color: #ECF5FF;
+}
+.rounded-2 {
+  border-radius: 12%;
+}
+.pink-font {
+  color: #FF99CD;
+}
+#arrow-from {
+  position: relative;
+}
+#arrow-to-extras {
+  position: absolute;
+  top: 6.8em;
+  right: 13em;
+  width: 5%;
+}
+ div {
+   border: 1px dotted pink;
+ }
+
+ p {
+   border: 1px solid green;
+ }
+
+ a {
+   border: 2px solid yellow
+ }
+
+ li {
+   border: 1px dashed cyan;
+ }
+
+ img {
+   border: 1px solid purple;
+ }
+
+ span {
+   border: 1px dashed brown;
+ }
+
+ button {
+   border: 1px dashed gray;
+ }
+
 </style>
