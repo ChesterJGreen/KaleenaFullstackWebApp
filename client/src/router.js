5870aa5cb0ed7809ef9e9fd1a87c091b769fed6f
ChesterJGreen <seejarom@gmail.com>
2024-03-21 23:37:42 -0600

    slight router mod

--- client/src/router.js
+++ client/src/router.js
@@ -42,8 +42,8 @@ const routes = [
       { path: 'qr19a', name: 'QR19A',component: loadPage('QR19APage') },
       { path: 'qr10pip', name: 'QR10PIP', component: loadPage('QR10PIPPage') },
       { path: 'qr11th', name: 'QR11TH', component: loadPage('QR11THPage') },
-      { path: 'qr21fy', name: 'QR21FY', component: loadPage('QR21FYPage') },
       { path: 'qr12d', name: 'QR12D', component: loadPage('QR12DPage') },
+      { path: 'qr21fy', name: 'QR21FY', component: loadPage('QR21FYPage') },
       { path: 'qr2gh', name: 'QR2GH', component: loadPage('QR2GHPage') }
     ]
   },
