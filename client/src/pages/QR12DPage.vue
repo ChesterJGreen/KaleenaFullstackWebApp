5f803268d97e3c519f60a3f42d7e98ffc7049731
ChesterJGreen <seejarom@gmail.com>
2024-03-21 23:22:01 -0600

    still need formatting on qr2gh

---
+++ client/src/pages/QR12DPage.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="playlist-font large-4x">Secret text</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <img class="" src="src\assets\img\1-Homepage Heart Divider.png" width="1000">
+            </div>
+        </div>
+        <div class="row px-5">
+            <div class="col-12 prata-font text-center mt-3">
+                <h5><u>Sarah's Nightmare</u></h5>
+            </div>
+            <div class="col-8 offset-2 volk-font mt-3 px-5">
+                <h5>
+                    <p> It was past midnight and the moon was giving her the needed light. Sarah pulled Gavin’s keys
+                        from her pocket and stepped up to the driver’s side of The Beast. Rowdy hadn’t been in Gavin’s
+                        room, allowing her to sneak the cowboy’s phone and keys without waking him. She prayed that the
+                        dog had fallen asleep somewhere out in the pastures.
+                        The thought must have jinxed her, because suddenly, Rowdy’s barking cut through the stillness of
+                        the night like a knife. Sarah turned to see him running towards her in the dim light of the
+                        moon. He halted to a stop at her feet.
+                        “Hush!” She held his snout closed with one hand while petting him with the other. “Quiet, boy.”
+                        Gavin had said he was well trained; she only needed to be firm. “Go to sleep in the barn.” She
+                        pointed and pushed him towards the red building.
+                        Rowdy barked again and Sarah cringed, frozen with dread. She looked at the house. No light
+                        turned on or door swung open. She breathed a sigh of relief and turned her attention back to
+                        Rowdy.
+                        “Quiet!” she commanded the dog once again. “Go sleep in the barn. Go, boy!” She motioned, but
+                        Rowdy didn’t move.
+                        She shoved the keys in her pocket and walked Rowdy to the barn. “Stay, boy.” She pointed to the
+                        ground and Rowdy sat on his haunches. “Good boy.” She petted him behind the ears. He barked once
+                        and Sarah couldn’t be sure if there’d been a noise by the house. “No bark! Now stay.”
+                        She turned and ran back to the truck. She needed to hurry. The keys got stuck in her pocket, and
+                        her heart dropped when they flipped out and hit the ground.
+                        “Shoot!”
+                        There was barely enough light to see properly and the dark ground hid the keys that would have
+                        been in plain sight had it been day. Her hands felt along the dirt and rock until they hit
+                        metal. The Beast’s engine would be noisy when she roared it to life, but she would be able to
+                        drive away before Gavin could do anything about it.
+                        A smile graced her lips over the thought, and she rose to pull open the truck door. Her hand
+                        grasped the metal and she yanked hard, but half a second later, an opposing force slammed the
+                        door shut.
+                        “Goin’ somewhere?” A deep voice next to her ear made her gasp, and her heart vaulted out of her
+                        chest.
+                        Sarah spun around to find Gavin’s hand holding the door closed. Before she could bolt, his other
+                        hand pressed against The Beast on the opposite side of her, pinning her to the window.
+                        She had been caught, and to make matters worse, Gavin was in nothing more than a pair of pajama
+                        pants, his bare chest just inches away from her. Her new red boots brought her up several inches
+                        closer to his face.
+                        All the moisture shriveled out of her throat and her tongue became thick. She made to duck under
+                        Gavin’s arm and run out into the darkness, but quick as a lasso on an unsuspecting calf, Gavin
+                        caught her.
+                        “Hey now,” he said, keeping his composure. “It’s okay, I’m not mad. Just glad I caught you.”
+                        Sarah’s breathing sped up at being so close together. But the sound of Rowdy’s bark snapped her
+                        out of it. He bounded towards them from the barn.
+                        “Good boy, Rowdy.” Gavin thanked his dog for the success. Had he purposely left Rowdy out of the
+                        house as a test? Rowdy barked again and nuzzled his face against their legs.
+                        Gavin’s eyes traveled over her. “Did I tell you how beautiful you look in this dress today?”
+                        Sarah looked down. Why had she not changed out of it? It was not a good getaway outfit.
+                        Gavin’s fingers traced the lace over her arms. “I can see your skin just here through the beams
+                        of moonlight.” The sensation stole her breath in a way she told herself she couldn’t let happen.
+                        His lack of anger threw her off guard. Why wasn’t he upset about this? Curse this cowboy and his
+                        dog.
+                        “Let me go.” Sarah wiggled. “Let me take the truck and go.”
+                        Gavin chuckled under his breath, holding her tighter. “I’m never,” he bent down closer to her
+                        ear, “letting you go.” He kissed the side of her neck, sending a shiver over her. “Sterling.” He
+                        kissed her neck again and this time the soft sensation made her close her eyes. “Sterling.”
+                        No—she couldn’t savor the feeling of his lips on her skin, but she did, too weakened by the
+                        sensation to push him away. He whispered as he kissed her again. “Sterling.”
+                        Why did he have to use that name while he kissed her?
+                        The reflection of sterling silver earrings caught her attention in the side mirror of the truck.
+                        Dark black makeup encircled her eyes and she watched as every kiss Gavin gave her crept closer
+                        to her lips. Her hands softened against his chest and black dye started creeping up the ends of
+                        her hair. Early morning sunbeams began to brighten the sky, and the passing of night took away
+                        her cares.
+                        His warm lips were almost to her mouth, the promised taste of heaven so close. Her anger had
+                        waned. It would be okay to stay a little longer.
+                        She blinked and saw her nails were painted black, matching the color change of her dress.
+                        Everything about her in the mirror’s reflection was black, but it was okay. Gavin’s mouth was
+                        almost to hers.
+                        “Sterling.”
+                        The brightness of the sun cresting the horizon blinded her and Sarah blinked hard.
+                        Wait, it wasn’t the sun that was blinding her. Her hand moved to cover her eyes. She was in her
+                        bedroom and Gavin was standing inside by the light switch.
+                        The escape had all been a dream. She’d tried to sneak into his room last night. Lingered on and
+                        off outside of his door, but he kept talking to Rowdy for some stupid reason. One of Sarah’s
+                        arms covered her eyes and the other threw a pillow at the cowboy. “Stay away from me.”
+                        “Geesh!” Gavin was ready for the day in his button-up shirt and jeans, and had even combed his
+                        hair extra nice. “It sounded like you were having a nightmare in here.”
+                        “I was, no thanks to you.” She pouted and rolled over.
+                        Rowdy barked from the hallway. “I sent Rowdy to wake you up, but apparently you were in such a
+                        deep sleep you didn’t hear his barkin’ through the door.”
+                        “Oh, I heard him alright,” she grumbled, and covered her head with her comforter. It wasn’t the
+                        fact that both in her dream and real life she had failed to escape. Or even the fact that the
+                        dream Gavin had weakened her resolve with his kisses. That happened in real life.
+                        No, it was succumbing to the blackness that scared her the most, making the dream her true
+                        nightmare.
+
+                    </p>
+                </h5>
+            </div>
+            <div class="col-8 offset-2">
+                <img src="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QR12D'
+}
+</script>
+<style scoped lang="scss"></style>
--- client/src/router.js
+++ client/src/router.js
@@ -42,9 +42,9 @@ const routes = [
       { path: 'qr19a', name: 'QR19A',component: loadPage('QR19APage') },
       { path: 'qr10pip', name: 'QR10PIP', component: loadPage('QR10PIPPage') },
       { path: 'qr11th', name: 'QR11TH', component: loadPage('QR11THPage') },
-      { path: '/qr21fy', component: loadPage('QR21FYPage') },
-      { path: '/qr12d', component: loadPage('QR12DPage') },
-      { path: '/qr2gh', component: loadPage('QR2GHPage') }
+      { path: 'qr21fy', name: 'QR21FY', component: loadPage('QR21FYPage') },
+      { path: 'qr12d', name: 'QR12D', component: loadPage('QR12DPage') },
+      { path: 'qr2gh', name: 'QR2GH', component: loadPage('QR2GHPage') }
     ]
   },
   {
